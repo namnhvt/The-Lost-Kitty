5efc5e5ec9fc65ba1f9786e61072e108843d16d0
namnhvt <130165236+namnhvt@users.noreply.github.com>
2025-05-01 12:37:03 +0700

    Add files via upload

--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -13,7 +13,7 @@ public class LoadSave {
 
 	public static final String PLAYER_ATLAS = "PlayerSpite.png";
 	public static final String LEVEL_ATLAS = "BackgroundLevel.png";
-	public static final String LEVEL_ONE_DATA = "level_one_data.png";
+	public static final String LEVEL_ONE_DATA = "TestStage.png";
 
 	public static BufferedImage GetSpriteAtlas(String fileName) {
 		BufferedImage img = null;
